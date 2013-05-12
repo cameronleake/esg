91d77aa6f875481c092474b8d04d2adc64bfaaa7
Cameron Leake <cameronleake7@gmail.com>
2013-05-12 14:15:34 +0930

    Updaing CarrierWave Image Storage Location for Production_Env #2

--- app/uploaders/featured_image_uploader.rb
+++ app/uploaders/featured_image_uploader.rb
@@ -19,14 +19,16 @@ class FeaturedImageUploader < CarrierWave::Uploader::Base
 
   CarrierWave.configure do |config|
     if Rails.env.production?
-      config.root = "/home/deployer/apps/esg/"
+      config.root = "/home/deployer/apps/esg/shared/assets/"
+      def store_dir
+        "assets/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+      end
     elsif Rails.env.development?
       config.root = "#{Rails.root}/public"
+      def store_dir
+        "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+      end
     end
-  end
-
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Generate Featured Image for Blog Post Blurb
