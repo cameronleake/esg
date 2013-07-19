714233bd43c947f9a1b07278e2da14479a54b411
Cameron Leake <cameronleake7@gmail.com>
2013-07-19 13:35:17 +1000

    Updating Nginx Config to Use SSL for HTTPS

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ if defined?(Bundler)
 end
 
 module Esg
-  class Application < Rails::Application
+  class Application < Rails::Application  
+    config.force_ssl = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
