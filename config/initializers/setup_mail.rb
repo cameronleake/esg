0af34615afdb2659168294f91984ea705a734e36
Cameron Leake <cameronleake7@gmail.com>
2013-07-20 12:17:20 +1000

    Adding New SMTP Server Settings

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,11 +2,11 @@ require 'development_mail_interceptor'
 
 ActionMailer::Base.smtp_settings = {
   :address              => "webcloud45.au.syrahost.com",
-  :port                 => 465,
+  :port                 => 25,
   :domain               => "engineeringsurvivalguide.com", 
   :user_name            => "noreply@engineeringsurvivalguide.com", 
   :password             => "survive99",
-  :authentication       => "plain",
+  :authentication       => :login,
   :enable_starttls_auto => true
 }
 
