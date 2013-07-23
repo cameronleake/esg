fef446429f4d56c4c02092f87045ce19b1dd5385
Cameron Leake <cameronleake7@gmail.com>
2013-07-23 16:31:29 +1000

    Updating Formatting of Index Page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,9 @@ class HomeController < ApplicationController
        
   
   def index
-    @recent_resources = Resource.order("created_at DESC").limit(NUMBER_RECENT_RESOURCES)      
+    @newest_resources = Resource.order("created_at DESC").limit(NUMBER_NEWEST_RESOURCES)      
+    @newest_articles = Article.order("created_at DESC").limit(NUMBER_NEWEST_ARTICLES)         
     @popular_resources = Resource.order("number_of_downloads DESC").limit(NUMBER_POPULAR_RESOURCES) 
-    @recent_articles = Article.order("created_at DESC").limit(NUMBER_RECENT_ARTICLES)         
   end                                      
                        
 end
